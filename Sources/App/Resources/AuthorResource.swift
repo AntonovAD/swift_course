45adb2c890cdd848c6d746914e7fcc2080ca3831
Антонов Андрей <an_42@mail.ru>
2020-10-23 20:07:46 +0700

    дособран ресур AuthorResource;

--- Sources/App/Resources/AuthorResource.swift
+++ Sources/App/Resources/AuthorResource.swift
@@ -2,8 +2,20 @@ import Vapor
 
 struct AuthorResource: Resource {
     let id: Int?
+    let lname: String
+    let fname: String
+    let userId: User.ID
+    let createdAt: Date?
+    let updatedAt: Date?
+    let deletedAt: Date?
 
     init(_ author: Author) {
         self.id = author.id
+        self.lname = author.lname
+        self.fname = author.fname
+        self.userId = author.userId
+        self.createdAt = author.createdAt
+        self.updatedAt = author.updatedAt
+        self.deletedAt = author.deletedAt
     }
 }
